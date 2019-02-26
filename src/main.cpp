1a527d1f4d25da2af0261dbfc6e995e6333e71f9
Ahmed Bakr <ahmed.m.ali.bakr@gmail.com>
2019-02-26 15:17:44 +0200

    - Update the main to work on linux.

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@ int main() {
 	Self_driving_car car(road_points);
 
 	h.onMessage([&car]
-		(uWS::WebSocket<uWS::SERVER> *ws, char *data, size_t length,
+		(uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
 			uWS::OpCode opCode) {
 		// "42" at the start of the message means there's a websocket message event.
 		// The 4 signifies a websocket message
@@ -116,30 +116,29 @@ int main() {
 
 					auto msg = "42[\"control\"," + msgJson.dump() + "]";
 
-					ws->send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+					ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
 				}  // end "telemetry" if
 			}
 			else {
 				// Manual driving
 				std::string msg = "42[\"manual\",{}]";
-				ws->send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+				ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
 			}
 		}  // end websocket if
 	}); // end h.onMessage
 
-	h.onConnection([&h](uWS::WebSocket<uWS::SERVER> *ws, uWS::HttpRequest req) {
+	h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
 		std::cout << "Connected!!!" << std::endl;
 	});
 
-	h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> *ws, int code,
+	h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
 		char *message, size_t length) {
-		ws->close();
+		ws.close();
 		std::cout << "Disconnected" << std::endl;
 	});
 
 	int port = 4567;
-	auto host = "127.0.0.1";
-	if (h.listen(host, port)) {
+	if (h.listen(port)) {
 		std::cout << "Listening to port " << port << std::endl;
 	}
 	else {
